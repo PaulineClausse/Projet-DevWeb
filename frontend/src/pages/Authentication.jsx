1ee100f05a0aaa2e9a692cb1bcbd93d8eec16272
pauline.clausse65@gmail.com <pauline.clausse65@gmail.com>
2025-06-18 16:28:44 +0200

    merge cookie with new account

--- frontend/src/pages/Authentication.jsx
+++ frontend/src/pages/Authentication.jsx
@@ -1,30 +1,36 @@
-import Navbar from "../components/Navbar";
 import { useState } from "react";
 import axios from "axios";
+import Navbar from "../components/Navbar";
 
 const Authentication = () => {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = async (e) => {
+    console.log("PUTOpd");
     e.preventDefault();
+
     try {
-      const res = await axios.post("http://localhost:5000/login", formData);
+      console.log("PUTOpd");
+      const response = await axios.post(
+        "http://localhost:5000/login",
+
+        {
+          email,
+          password,
+        },
+        { withCredentials: true }
+      );
       window.location.href = "/home";
+
+      console.log("Login successful:", response.data);
     } catch (err) {
-      console.error(err);
-      alert(err.response?.data?.message || "Erreur lors de l’enregistrement");
+      console.error("Login failed:", err.response?.data || err.message);
+
+      alert("Échec de connexion.");
     }
   };
+
   return (
     <div>
       <div className="min-h-screen bg-[rgb(38,38,38)] flex flex-col items-center justify-center px-4">
@@ -33,14 +39,13 @@ const Authentication = () => {
             Connexion
           </h2>
 
-          <form className="space-y-4" onSubmit={handleLogin}>
+          <form onSubmit={handleLogin} className="space-y-4">
             <div>
               <label className="block text-gray-700 text-sm mb-1">Email</label>
               <input
                 type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="ex: utilisateur@mail.com"
               />
@@ -52,9 +57,8 @@ const Authentication = () => {
               </label>
               <input
                 type="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="••••••••"
               />
@@ -69,10 +73,7 @@ const Authentication = () => {
           </form>
 
           <p className="text-sm text-center text-gray-600 mt-4">
-            Pas encore de compte ?{" "}
-            <a href="/register" className="text-blue-600 hover:underline">
-              Créer un compte
-            </a>
+            Pas encore de compte ?
           </p>
         </div>
       </div>
